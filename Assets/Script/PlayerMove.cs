71e4805fc2b95894d7a098a4a3f4a5879d05bef1
Indulgein <1827025287@qq.com>
2024-04-13 16:21:17 +0800

    优化可以无限跳跃，限制踩到地板才能跳跃

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -10,10 +10,13 @@ public class PlayerMove : MonoBehaviour
     private bool FaceRight=true;
     private Animator myAnim;
     [SerializeField] float JumpSpeed;
+    private BoxCollider2D myFeet;
+    private bool isGround;
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();//导入2D角色碰撞刚体
         myAnim=GetComponent<Animator>();//获取角色行为
+        myFeet=GetComponent<BoxCollider2D>();//获取角色脚是否在地面
     }
  
     void Update()
@@ -23,11 +26,17 @@ public class PlayerMove : MonoBehaviour
         Jump();//Update函数受帧率影响，每帧刷新时捕获玩家按键输入，随着帧率的改变变快或变慢。
         //FixedUpdate函数则不受帧率影响，固定频率，因为跳跃需要立即检测并回应
         //所以固定频率对于跳跃的检测不太友好。
+        CheckGround();
     }
     private void FixedUpdate()
     {
         Move();
         Run();
+    }
+     void CheckGround()
+    {
+        isGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        Debug.Log(isGround);
     }
     private void Move()
     {   
@@ -43,7 +52,7 @@ public class PlayerMove : MonoBehaviour
     }
     private void Run()
     {
-        //RUN行为
+       
         bool playerHasXAxisSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;//检测角色X轴的速度是否大于0小于1
         myAnim.SetBool("IsRun", playerHasXAxisSpeed);//通过前面的判定对IsRun赋值
     }
@@ -51,8 +60,10 @@ public class PlayerMove : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            Vector2 jumpV1 = new Vector2(0.0f, JumpSpeed);
-            rb.velocity=Vector2.up*jumpV1;
+            if (isGround) {
+                Vector2 jumpV1 = new Vector2(0.0f, JumpSpeed);//创建跳跃速度初始化为0
+                rb.velocity = Vector2.up * jumpV1;//把刚体速度设置成跳跃速度向量
+            }
         }
     }
 }
